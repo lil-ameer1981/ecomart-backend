ff83c464439c8424032f4a57156f5d6dad325bd2
Ameer <126087869+lil-ameer1981@users.noreply.github.com>
2025-12-09 16:23:26 +0400

    Fix syntax error in server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 // Initialize Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.listen(PORT)
+app.listen(PORT);
 
 // MongoDB connection
 let db;
